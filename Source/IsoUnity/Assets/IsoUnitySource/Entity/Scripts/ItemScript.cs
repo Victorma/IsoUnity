6f3fe52f8a407b57286880a1e6757c50f75097ea
Victormafire <victorma-fire@hotmail.com>
2014-03-26 01:40:23 +0100

    Items and inventories real commit?

---
+++ Source/IsoUnity/Assets/IsoUnitySource/Entity/Scripts/ItemScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScript : EntityScript {
+
+	public Item item;
+
+	private bool picked = false;
+	private Inventory by;
+
+	public override void eventHappened (GameEvent ge)
+	{
+		if (ge.getParameter ("Item") == this.item) {
+			switch(ge.Name.ToLower()){
+			case "pick":break;
+			}
+		}
+	}
+
+	public override Option[] getOptions ()
+	{
+		GameEvent ge = new GameEvent ();
+		ge.Name = "Pick";
+		ge.setParameter ("Item", this.item);
+
+		return new Option[]{new Option ("Pick", ge)};
+
+	}
+
+	public override void tick ()
+	{
+		if (item == null) {
+		
+		}
+		throw new System.NotImplementedException ();
+	}
+
+	public override void Update ()
+	{
+		throw new System.NotImplementedException ();
+	}
+}
