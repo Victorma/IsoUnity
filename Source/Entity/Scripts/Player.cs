ff2057e9ea28202697582cf65aea326d1f8334f6
Victorma Perez Colado <victorma-fire@hotmail.com>
2015-07-06 16:17:29 +0200

    Player changes
    + Commented (hope it helps).
    + Added an active Flag (boolean variable) to activate the response of
    the player. (Default is active). (Closes #27)
    + Added a line to verfy we can move to cell before we launch the
    movement event on key pressed in order to assure we only allow direct
    movement (without route calculation) (Fixes #29)

--- Source/Entity/Scripts/Player.cs
+++ Source/Entity/Scripts/Player.cs
@@ -5,9 +5,12 @@ public class Player : EntityScript {
 	
 	public override void eventHappened (GameEvent ge)
 	{
+        // If we're waiting for the event finished
 		if(movement!=null)
 			if(ge.Name.ToLower() == "event finished")
+                // Let's check it's our event:
 				if(ge.getParameter("event") == movement){
+                    // Ok it's done
 					movement = null;
 				}
 	}
@@ -15,13 +18,38 @@ public class Player : EntityScript {
 	private GameEvent movement;
 	private GameEvent toLaunch;
 
+    [SerializeField]
+    private bool active = true;
+    public bool Active
+    {
+        get
+        {
+            return active;
+        }
+        set
+        {
+            this.active = value;
+        }
+    }
+
 	public void onControllerEvent(ControllerEventArgs args){
+        // # Avoid responding controller event when inactive
+        if (!active)
+            return;
+
+        // # Normal threatment
+        // Multiple controller events only give one launch result per tick
 		if(toLaunch == null){
+            // If options received (from entities)
 			if( args.options != null){
+                // If there's only one, proceed to launch
 				if(args.options.Length==1){
+                    // The action is the event we have to launch, 
+                    // so in order to know who's launching, we put a mark
 					if(args.options[0].Action!=null)
 						args.options[0].Action.setParameter("Executer", this.Entity);
 
+                    // If we've to move to perform the action
 					if(args.options[0].HasToMove){
 						GameEvent ge = ScriptableObject.CreateInstance<GameEvent>();
 						ge.setParameter("entity", this.Entity);
@@ -31,22 +59,31 @@ public class Player : EntityScript {
 						ge.Name = "move";
 						movement = ge;
 						Game.main.enqueueEvent(ge);	
+
+                        // Here we've launched the movement. As it's synchronous, we'll receive 
+                        // the movement finished when the Mover considers it's done.
 					}
 
 					toLaunch = args.options[0].Action;
-				}else if(args.options.Length > 1){
+				}
+                // If there're multiple actions we have to display a menu so player can choose
+                else if(args.options.Length > 1){
 					OptionsGUI gui = ScriptableObject.CreateInstance<OptionsGUI>();
 					gui.init(args, Camera.main.WorldToScreenPoint(args.entity.transform.position), args.options);
 					GUIManager.addGUI(gui, 100);
 				}
-
-			}else if(args.cell != null){
+			}
+            // If the argument doesn't contain options but it has a cell, we'll try to move over there
+            else if(args.cell != null){
 				GameEvent ge = ScriptableObject.CreateInstance<GameEvent>();
 				ge.setParameter("entity", this.Entity);
 				ge.setParameter("cell", args.cell);
 				ge.Name = "move";
 				Game.main.enqueueEvent(ge);			
-			}else {				
+			}
+            // Otherwise, the controller event should contain keys pressed
+            else {
+		
 				int to = -1;
 				if(args.LEFT){ to = 0; }
 				else if(args.UP){ to = 1;}
@@ -56,12 +93,16 @@ public class Player : EntityScript {
 				if(to > -1){
 					if(Entity == null)
 						Debug.Log ("Null!");
-					Cell destino = Entity.Position.Map.getNeightbours(Entity.Position)[to];
-					GameEvent ge = ScriptableObject.CreateInstance<GameEvent>();
-					ge.setParameter("entity", this.Entity);
-					ge.setParameter("cell", destino);
-					ge.Name = "move";
-					Game.main.enqueueEvent(ge);
+					Cell destination = Entity.Position.Map.getNeightbours(Entity.Position)[to];
+                    // Can move to checks if the entity can DIRECT move to this cells.
+                    // This should solve bug #29
+                    if (this.Entity.canMoveTo(destination)) {
+                        GameEvent ge = ScriptableObject.CreateInstance<GameEvent>();
+                        ge.setParameter("entity", this.Entity);
+                        ge.setParameter("cell", destination);
+                        ge.Name = "move";
+                        Game.main.enqueueEvent(ge);
+                    }
 				}
 			} 
 		}
@@ -72,7 +113,9 @@ public class Player : EntityScript {
 	public override void tick(){
 
 		if(toLaunch != null){
+            // If we're not waiting to receive event finished from Mover.
 			if(movement == null){
+                // That means that, we're in the right position so...
 				Game.main.enqueueEvent(toLaunch);
 				toLaunch = null;
 			}
