c9202ac8c71b1c90fb001c5ca8c0941a1ec0ef2c
Victorma Perez Colado <victorma-fire@hotmail.com>
2015-08-06 07:09:26 +0200

    Travis configs

---
+++ Scripts/build.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
+# Change this the name of your project. This will be the name of the final executables as well.
+project="IsoUnity"
+
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -exportPackage Assets\IsoUnity IsoUnity
+  -quit
+
+echo 'Logs from build'
+cat $(pwd)/unity.log
