5e4d408540b864f34d853b4dd446d346c5a84cdd
Victorma Perez Colado <victorma-fire@hotmail.com>
2015-08-07 01:15:00 +0200

    Some more changes for travis

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,9 +8,13 @@ project="IsoUnity"
 echo "Creating Build dir"
 mkdir -p $(pwd)/Build/
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -silent-crashes -logFile $(pwd)/unity.log -projectPath $(pwd) -exportPackage Assets/IsoUnity "$(pwd)/Build/$project.unitypackage" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath $(pwd) -exportPackage Assets/IsoUnity "$(pwd)/Build/$project.unitypackage" -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
+echo 'Root file dir:'
+ls -la $(pwd)
 echo 'Build file dir:'
 ls -la $(pwd)/Build/
+echo 'Temp file dir:'
+ls -la $(pwd)/Temp/
