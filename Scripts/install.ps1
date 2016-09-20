8fcb271d37f222cb70847779e0b3fa0ee7bc23bd
Victorma Perez Colado <victorma-fire@hotmail.com>
2016-09-20 23:50:03 +0200

    Install changed

--- Scripts/install.ps1
+++ Scripts/install.ps1
@@ -1,5 +1,8 @@
+$unitysetup = "UnitySetup64.exe"
+$unitysetupargs = "/S"
+
 Write-Output 'Downloading from http://netstorage.unity3d.com/unity/649f48bbbf0f/Windows64EditorInstaller/UnitySetup64-5.4.1f1.exe: '
-curl -o UnitySetup.exe http://netstorage.unity3d.com/unity/649f48bbbf0f/Windows64EditorInstaller/UnitySetup64-5.4.1f1.exe
+curl -o $unitysetup http://netstorage.unity3d.com/unity/649f48bbbf0f/Windows64EditorInstaller/UnitySetup64-5.4.1f1.exe
 
 Write-Output 'Installing UnitySetup64.exe'
-UnitySetup.exe /S
+Start-Process $unitysetup $unitysetupargs -Wait 
