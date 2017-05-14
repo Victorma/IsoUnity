c95dfb97121b1e18626f7b87ada73680729d255d
Victorma Perez Colado <victormp@ucm.es>
2017-05-14 17:02:14 +0200

    Added TimerManager

---
+++ Assets/IsoUnity/Source/EvenManagers/TimerManager.cs
@@ -0,0 +1,20 @@
+﻿using IsoUnity.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IsoUnity
+{
+    public class TimerManager : EventedEventManager
+    {
+
+        [GameEvent(true,false)]
+        public IEnumerator Wait(float time, bool realtime = false)
+        {
+            if(realtime)
+                yield return new WaitForSecondsRealtime(time);
+            else
+                yield return new WaitForSeconds(time);
+        }
+    }
+}
