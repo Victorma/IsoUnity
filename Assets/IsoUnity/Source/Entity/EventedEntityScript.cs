b95db888f9172af8b576a8328bfc190c5429add4
Victorma Perez Colado <victormp@ucm.es>
2017-05-14 16:51:20 +0200

    EventedEventManager + EventedEntityScript

--- Assets/IsoUnity/Source/Entity/EventedEntityScript.cs
+++ Assets/IsoUnity/Source/Entity/EventedEntityScript.cs
@@ -11,41 +11,18 @@ namespace IsoUnity.Entities
     {
         private bool inited = false;
         private Dictionary<GameEventConfig, MethodInfo> calls;
-        private Dictionary<MethodInfo, bool> autofinish;
+        private Dictionary<MethodInfo, GameEventAttribute> attrInfo;
 
         public override void eventHappened(IGameEvent ge)
         {
             Current = ge;
 
-            if(calls != null && calls.Count > 0)
-            {
-                var config = new GameEventConfig(ge);
-
-                if (ge.belongsTo(this, this.GetType().Name) && calls.ContainsKey(config))
-                {
-                    List<object> parameters = new List<object>();
-                    var call = calls[config];
-                    foreach (var p in call.GetParameters())
-                        if (ge.Params.Contains(p.Name.ToLower()))
-                            parameters.Add(ge.getParameter(p.Name));
-                        else parameters.Add(p.DefaultValue);
-                    
-                    var output = call.Invoke(this, parameters.ToArray());
-
-                    if(output is IEnumerator)
-                        // If we want to autofinish it we use the controller, else just launch it
-                        StartCoroutine(autofinish[call] ? CoroutineController(ge, output as IEnumerator) : output as IEnumerator);
-                    else if (autofinish[call])
-                        // If is not a coroutine and we have to auto finish it, we just do it
-                        Game.main.eventFinished(ge);
-
-                }
-            }
+            EventHappened(this, calls, attrInfo, ge);
 
             Current = null;
         }
 
-        private IEnumerator CoroutineController(IGameEvent ge, IEnumerator toRun)
+        private static IEnumerator CoroutineController(IGameEvent ge, IEnumerator toRun)
         {
             // We wrap the coroutine
             while (toRun.MoveNext())
@@ -66,19 +43,54 @@ namespace IsoUnity.Entities
         {
             if (!inited)
             {
-                calls = new Dictionary<GameEventConfig, MethodInfo>();
-                autofinish = new Dictionary<MethodInfo, bool>();
-
-                foreach (var method in this.GetType().GetMethods().ToList()
-                      .FindAll(m => m.GetCustomAttributes(typeof(GameEventAttribute), true).Length > 0)
-                      .ToArray())
-                {
-                    
-                    autofinish.Add(method, ((GameEventAttribute)method.GetCustomAttributes(typeof(GameEventAttribute), true)[0]).AutoFinish);
-                    calls.Add(new GameEventConfig(this.GetType(), method), method);
-                }
-
+                Init(this.GetType(), ref calls, ref attrInfo);
                 inited = true;
+            }
+        }
+
+        internal static void Init(Type type, ref Dictionary<GameEventConfig, MethodInfo> calls, ref Dictionary<MethodInfo, GameEventAttribute> attrInfo)
+        {
+            calls = new Dictionary<GameEventConfig, MethodInfo>();
+            attrInfo = new Dictionary<MethodInfo, GameEventAttribute>();
+
+            foreach (var method in type.GetMethods().ToList()
+                    .FindAll(m => m.GetCustomAttributes(typeof(GameEventAttribute), true).Length > 0)
+                    .ToArray())
+            {
+                var attr = ((GameEventAttribute)method.GetCustomAttributes(typeof(GameEventAttribute), true)[0]);
+                attrInfo.Add(method, attr);
+                calls.Add(new GameEventConfig(type, method), method);
+            }
+        }
+
+        internal static void EventHappened(MonoBehaviour reference, Dictionary<GameEventConfig, MethodInfo> calls, Dictionary<MethodInfo, GameEventAttribute> attrInfo, IGameEvent ge)
+        {
+            if (calls != null && calls.Count > 0)
+            {
+                var config = new GameEventConfig(ge);
+                if (calls.ContainsKey(config))
+                {
+                    var call = calls[config];
+                    
+                    if (!attrInfo[call].RequiresReference || ge.belongsTo(reference, reference.GetType().Name))
+                    {
+                        List<object> parameters = new List<object>();
+                        foreach (var p in call.GetParameters())
+                            if (ge.Params.Contains(p.Name.ToLower()))
+                                parameters.Add(ge.getParameter(p.Name));
+                            else parameters.Add(p.DefaultValue);
+
+                        var output = call.Invoke(reference, parameters.ToArray());
+
+                        if (output is IEnumerator)
+                            // If we want to autofinish it we use the controller, else just launch it
+                            reference.StartCoroutine(attrInfo[call].AutoFinish ? CoroutineController(ge, output as IEnumerator) : output as IEnumerator);
+                        else if (attrInfo[call].AutoFinish)
+                            // If is not a coroutine and we have to auto finish it, we just do it
+                            Game.main.eventFinished(ge);
+
+                    }
+                }
             }
         }
     }
@@ -87,9 +99,11 @@ namespace IsoUnity.Entities
     public class GameEventAttribute : System.Attribute
     {
         public bool AutoFinish { get; private set; }
-        public GameEventAttribute(bool autoFinish = true)
+        public bool RequiresReference { get; private set; }
+        public GameEventAttribute(bool autoFinish = true, bool requiresReference = true)
         {
             AutoFinish = autoFinish;
+            RequiresReference = requiresReference;
         }
     }
 
@@ -102,6 +116,8 @@ namespace IsoUnity.Entities
         public Dictionary<string, bool> ParameterHasDefault { get; private set; }
         public Dictionary<string, object> DefaultValue { get; private set; }
 
+        public bool RequiresReference { get; private set; }
+
         public GameEventConfig(Type t, MethodInfo m)
         { 
             Name = SplitCamelCase(m.Name).ToLower();
@@ -109,8 +125,15 @@ namespace IsoUnity.Entities
             ParameterHasDefault = new Dictionary<string, bool>();
             DefaultValue = new Dictionary<string, object>();
 
-            ParameterConfig.Add(t.Name.ToLower(), t);
-            ParameterHasDefault.Add(t.Name.ToLower(), false);
+            var attr = ((GameEventAttribute)m.GetCustomAttributes(typeof(GameEventAttribute), true)[0]);
+
+            if (attr.RequiresReference)
+            {
+                ParameterConfig.Add(t.Name.ToLower(), t);
+                ParameterHasDefault.Add(t.Name.ToLower(), false);
+            }
+
+            RequiresReference = attr.RequiresReference;
 
             foreach (var parameter in m.GetParameters())
             {
