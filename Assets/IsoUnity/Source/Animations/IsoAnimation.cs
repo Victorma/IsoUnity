1c593d5eec109237aed9e08dbc2dc2f2700bd9d0
Victorma Perez Colado <victormp@ucm.es>
2018-05-30 01:05:32 +0200

    Added missing IsoAnimation

---
+++ Assets/IsoUnity/Source/Animations/IsoAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IsoUnity 
+{
+	[System.Serializable]
+	[CreateAssetMenu(fileName = "new IsoAnimation", menuName = "IsoUnity/IsoAnimation")]
+	public class IsoAnimation : ScriptableObject {
+
+		[System.Serializable]
+		public struct Frame {
+			public int column;
+			public float duration;
+		}
+
+		[SerializeField]
+		public bool loop = false;
+		[SerializeField]
+		public List<Frame> frames = new List<Frame>();
+		[SerializeField]
+		public string sheet;
+		[SerializeField]
+		public IsoDecoration overrideSheet;
+	}
+}
+
